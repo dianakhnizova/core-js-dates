ca4ccfae3afd9465e5fc8c1c680bca63a42d723d
dianakhnizova <khnizovad@gmail.com>
2025-03-04 13:13:24 +0600

    feat: complete task - getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -218,8 +218,15 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const date2 = new Date(date);
+  date2.setDate(13);
+
+  while (date2.getDay() !== 5) {
+    date2.setMonth(date2.getMonth() + 1);
+  }
+
+  return date2;
 }
 
 /**
