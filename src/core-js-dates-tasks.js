6a0641a87e35500736550f3899b03764ddac55a8
dianakhnizova <khnizovad@gmail.com>
2025-03-04 12:06:55 +0600

    feat: complete task - getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -72,8 +72,14 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const date2 = new Date(date);
+  const dayNum = date2.getDay();
+  const dayUntilFriday = (5 - dayNum + 7) % 7 || 7;
+
+  date2.setDate(date2.getDate() + dayUntilFriday);
+
+  return date2;
 }
 
 /**
